9d4263177d79a2837151336d399e2aea35ffe302
O3-O2O <hunghrbest@gmail.com>
2026-02-14 15:15:57 +0700

    Add files via upload

--- script/quiz.js
+++ script/quiz.js
@@ -69,3 +69,94 @@ window.loading = async () => {
 
 
 loading()
+
+const guessForm = document.getElementById("guess-open");
+const guessBtn = document.getElementById("guess-btn");
+const inputGuess = document.querySelector(".input-guess");
+
+let text = 0
+
+// ===== MỞ FORM =====
+function openGuess(){
+    guessForm.classList.remove("hidden");
+    document.body.style.overflow = "hidden"; // khóa scroll
+    setTimeout(() => {
+        inputGuess.focus();
+    }, 200);
+}
+
+// ===== ĐÓNG FORM =====
+function closeGuess(){
+    guessForm.classList.add("hidden");
+    document.body.style.overflow = "auto"; // mở scroll lại
+    inputGuess.value = "";
+}
+
+// Click nút tròn để mở
+guessBtn.addEventListener("click", openGuess);
+
+// Click ra ngoài để đóng
+document.addEventListener("click", function(e){
+    if(!guessForm.contains(e.target) && 
+       !guessBtn.contains(e.target) && 
+       !guessForm.classList.contains("hidden")){
+        closeGuess();
+    }
+});
+
+// Nhấn ESC để đóng
+document.addEventListener("keydown", function(e){
+    if(e.key === "Escape"){
+        closeGuess();
+    }
+});
+
+// Nhấn Enter để submit
+inputGuess.addEventListener("keydown", function(e){
+    if(e.key === "Enter"){
+        e.preventDefault();
+        document.getElementById("guessing").click();
+    }
+});
+
+const guessing = document.getElementById("guessing");
+
+const winner = new Audio("../soundtrack/TT_đúng_O7.mp3.mp3")
+const loser = new Audio("../soundtrack/TT_mở_đáp_án_left_O11.mp3")
+
+guessing.addEventListener("click", () => {
+
+    const userAnswer = document.getElementById("input-guess").value.trim().toLowerCase();
+
+    fetch("https://68ce57f66dc3f350777eb99b.mockapi.io/olympia")
+        .then(res => res.json())
+        .then(data => {
+
+            const accept_guess = data[5].dung.toLowerCase();
+
+            if(userAnswer === accept_guess){
+
+                // Mở toàn bộ ảnh
+                for(let i = 1; i <= 5; i++){
+                    const block = document.getElementById(`quiz-${i}`);
+                    if(block){
+                        block.style.display = "none";
+                    }
+                }
+
+                winner.play()
+
+                closeGuess();
+
+            } else {
+
+                loser.play()
+                document.getElementById("input-guess").value = "";
+
+                closeGuess();
+
+            }
+
+        });
+
+});
